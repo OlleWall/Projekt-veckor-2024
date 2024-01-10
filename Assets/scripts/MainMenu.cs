889a11f72f7c059736cf9dedfd830964b6330280
JaggedSwords <97683126+JaggedSwords@users.noreply.github.com>
2024-01-10 11:56:33 +0100

    Main menu changed

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("NEXT-SCENE-1");
+    }
+
+    public void OptionScreen()
+    {
+        SceneManager.LoadScene("NEXT-SCENE-2");
+    }
+
+    public void CreditScreen()
+    {
+        SceneManager.LoadScene("NEXT-SCENE-3");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
