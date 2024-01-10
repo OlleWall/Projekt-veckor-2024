1f9215f471ba9de3b169a1edd55d274b8afd00ec
OlleWall <97678822+OlleWall@users.noreply.github.com>
2024-01-10 13:41:51 +0100

    player movement klar

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -18,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(0.01f, 1f)]
     float coyoteTime = 0.05f; // hur länge innan man förlorar sitt hopp efter spelarn slutar nudda marken
 
+    float coyoteTimer; // själva variabeln som räknar upp
+
+    bool canJump; // om man kan hoppa
+
     [SerializeField]
     BoxCollider2D crouchCollider; // collidern som ska disableas när man crouchar
 
@@ -27,20 +31,33 @@ public class PlayerMovement : MonoBehaviour
 
     bool crouching = false;
 
-    public LayerMask groundMask; // layermasken för marken
+    [SerializeField]
+    LayerMask groundMask; // layermasken för marken
 
-    bool airJump; // om man kan hoppa
+    [SerializeField]
+    LayerMask ladderMask; // layermasken för stegar
 
+    public bool climbing = false;
+
+    [SerializeField, Range(0, 10)]
+    float climbingSpeed = 3; // hastigheten när man klätrar
+
+    int facingRight;
+
+    float gravity;
 
     void Start()
     {
         // hämtar rigidbody componenten
         rb2D = GetComponent<Rigidbody2D>();
 
+        // sätter gravity till gravityn på rigidbodyn
+        gravity = rb2D.gravityScale;
+
         // hämtar boxcollidern som då är den övre
         crouchCollider = GetComponent<BoxCollider2D>();
 
-        // definerar livespeed
+        // sätter liveSpeed till den vanliga hastigheten
         liveSpeed = speed;
     }
 
@@ -50,37 +67,78 @@ public class PlayerMovement : MonoBehaviour
         // uppdaterar inputX med inputen i horizontella riktningen
         inputX = Input.GetAxisRaw("Horizontal");
 
-        // om spelaren är på marken ska spelaren röra sig snabbare och tätare annars ska den ändra hastighet långsamt
-        if (GroundCheck())
+        // uppdaterar facingRight genom att om input blir större eller mindre än 0 ska den sätta värdet på facingRight efter det.
+        if (inputX > 0)
         {
-            rb2D.velocity = new Vector2(liveSpeed * inputX, rb2D.velocity.y);
+            facingRight = 1;
         }
-        // kollar så spelaren bara accelererar när den är under hur snabbt man går på marken
-        else if (rb2D.velocity.x < inputX * speed)
+        else if (inputX < 0)
         {
-            rb2D.AddForce(new Vector2(speed * 0.2f * inputX, 0), ForceMode2D.Force);
+            facingRight = -1;
         }
-        
-        // kollar om W trycks ned
-        if (Input.GetKey(KeyCode.W))
+
+        // bara när man inte klätrar ska man kunna göra dessa saker
+        if (!climbing)
         {
-            // kollar om spelaren inte redan har hoppat sedan man nuddade marken, det var 0.15 sekunder sedan man hoppa och om spelaren inte flyter
-            if (airJump == true)
+            // om spelaren är på marken ska spelaren röra sig snabbare och tätare annars ska den ändra hastighet långsamt
+            if (GroundCheck())
             {
-                // stannar spelaren på y axeln och trycker upp spelaren
-                rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
-                rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
-                // säger att man har hoppat i luften
-                airJump = false;
+                rb2D.velocity = new Vector2(liveSpeed * inputX, rb2D.velocity.y);
+            }
+            // kollar så spelaren bara accelererar när den är under hur snabbt man går på marken
+            else if (rb2D.velocity.x < inputX * speed)
+            {
+                rb2D.AddForce(new Vector2(speed * 0.2f * inputX, 0), ForceMode2D.Force);
+            }
+
+            // kollar om W eller space trycks ned
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+            {
+                // kollar om spelaren inte redan har hoppat sedan man nuddade marken, det var 0.15 sekunder sedan man hoppa och om spelaren inte flyter
+                if (canJump == true)
+                {
+                    // trycker upp spelaren
+                    rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+                    // säger att man har hoppat och inte can längre
+                    canJump = false;
+                }
+            }
+
+            // när vänster ctrl trycks ned och man inte är crouchar under ett tak så byter den crouch boolen till motsatta
+            if (Input.GetKeyDown(KeyCode.LeftControl) && !CelingCheck())
+            {
+                crouching = !crouching;
+            }
+        }       
+        else
+        {
+            crouching = false;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb2D.velocity = new Vector2(0, climbingSpeed);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rb2D.velocity = new Vector2(0, -climbingSpeed);
+            }
+            else
+            {
+                rb2D.velocity = new Vector2(0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb2D.velocity = new Vector2(speed * -facingRight, jumpForce);
+                facingRight *= -1;
+                
+            }
+            else if (!LadderCheck())
+            {
+                // när man slutar nudda stegen så hoppar man fram och upp
+                rb2D.velocity = new Vector2(crouchSpeed * facingRight, 5);
             }
         }
-
-        // när vänster ctrl trycks ned och man inte är crouchar under ett tak så byter den crouch boolen till motsatta
-        if (Input.GetKeyDown(KeyCode.LeftControl) && !CelingCheck())
-        {
-            crouching = !crouching;
-        }
-
         // när crouching är true disableas crouchCollider som är den övre collidern och liveSpeed byts till crouchSpeed som är långsammare
         if (crouching)
         {
@@ -94,18 +152,44 @@ public class PlayerMovement : MonoBehaviour
             liveSpeed = speed;
         }
 
-        // kollar functionen GroundCheck och får ett true false värde tillbaka
+        // använder GroundCheck för att kolla om spelaren är på marken och om den är det sätts coyoteTimer till 0
         if (GroundCheck())
         {
-            // säger att spelaren kan hoppa
-            airJump = true;
+            coyoteTimer = 0;
+        }
+        // om spelaren inte är på marken så ska den räkna upp på coyoteTimer
+        else
+        {
+            coyoteTimer += Time.deltaTime;
+        }
+        
+        // om coyoteTimer är större än coyoteTime så ska canJump bli false
+        if (coyoteTime <= coyoteTimer)
+        {
+            canJump = false;
+        }
+        // annars ska canJump vara true
+        else
+        {
+            canJump = true;
+        }
+
+        if (LadderCheck())
+        {
+            climbing = true;
+            rb2D.gravityScale = 0;
+        }
+        else
+        {
+            climbing = false;
+            rb2D.gravityScale = gravity;
         }
     }
 
     bool GroundCheck()
     {
-        // skapar en raycast som börjar i spelaren, åker neråt och kollar om den träffar något i Floor layer och sparar detta i en hit varibael
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position - new Vector3(0, 0.95f, 0), 0.15f, -Vector2.up, 1f, groundMask);
+        // skapar en raycast som börjar i spelaren, kollar om den träffar något i Ground layer och sparar detta i en hit variabeln
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position - new Vector3(0, 0.95f, 0), new Vector2(0.6f, 0.3f), 0, Vector2.up, 0, groundMask);
 
         // om raycasten har träffat någontig och punkten där den träffa är mindre än 0.6 units från spelaren
         if (hit.transform != null && hit.distance < 0.025F)
@@ -113,51 +197,52 @@ public class PlayerMovement : MonoBehaviour
             // skickar den true
             return true;
         }
-        // om det inte är sant
-        else
-        {
-            // skickar den false
-            return false;
-        }
+        // annar false
+        return false;
     }
 
     bool CelingCheck()
     {
-        RaycastHit2D[] hit = Physics2D.BoxCastAll(transform.position + new Vector3(0, 0.5f, 0), new Vector2(0.9f, 1), 0, Vector2.up, 0, groundMask);
+        // skapar en BoxCast som kollar efter object i ground layer och sparar den i hit
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + new Vector3(0, 0.5f, 0), new Vector2(0.9f, 1), 0, Vector2.up, 0, groundMask);
 
-        foreach (RaycastHit2D item in hit)
+        // om hit har en transform skickar den true 
+        if (hit.transform != null)
         {
-            if (item.transform != null)
-            {
-                return true;
-            }
+            return true;
         }
 
+        // annars false
         return false;
     }
 
-    bool WallCheck()
+    bool LadderCheck()
     {
-        RaycastHit2D[] hit = Physics2D.BoxCastAll(transform.position + new Vector3(0.01f * inputX, 0, 0), new Vector2(1f, 1.9f), 0, Vector2.up);
+        // skapar en BoxCast som kollar efter object i ground layer och sparar den i hit
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + new Vector3(0.1f * facingRight, 0, 0), new Vector2(1f, 1.9f), 0, Vector2.up, 0, ladderMask);
 
-        foreach (RaycastHit2D item in hit)
+        // om hit har en transform skickar den true 
+        if (hit.transform != null)
         {
-            if (item.collider.gameObject.layer != gameObject.layer)
-            {
-                print(item.collider.gameObject.layer);
-                return true;
-            }
+            return true;
         }
-        print("hello");
+
+        // annars false
         return false;
     }
 
     private void OnDrawGizmos()
     {
+        // Crouchcheck raycast gizmo
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position + new Vector3(0, 0.5f, 0), new Vector2(0.9f, 1));
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(transform.position + new Vector3(0.01f * inputX, 0, 0), new Vector2(1.1f, 1.9f));
 
+        // GroundCheck raycast gizmo
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position - new Vector3(0, 0.95f, 0), new Vector3(0.6f, 0.3f));
+
+        // Laddercheck raycast gizmo
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position + new Vector3(0.1f * facingRight, 0, 0), new Vector2(1f, 1.9f));
     }
 }
