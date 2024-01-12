2d90148eb7b2373e8fdb702f0fdb208ffca52011
GamingPhoenix04 <marcus.picha@elev.ga.lbs.se>
2024-01-12 09:13:08 +0100

    offset och finslipad layermovement script

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -6,14 +6,17 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player;  // Reference to the player's transform
     public float smoothSpeed = 0.125f;  // Smoothness of camera movement
+    public Vector3 offset = new Vector3(0f, 4.2f, -9.2f); // Offset for the camera
+
 
     void LateUpdate()
     {
         if (player != null)
         {
-            Vector3 desiredPosition = player.position;
+            Vector3 desiredPosition = player.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+            
         }
     }
 }
