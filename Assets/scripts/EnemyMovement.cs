4ceaefeccc6a253c7f7f80a274d105235a459495
20Simon17 <98454367+20Simon17@users.noreply.github.com>
2024-02-13 13:14:30 +0100

    vision cone (simkon scene)
    klar med vision cone så att den kan anpassas för olles system senare (kopierad kod tyvärr)

--- Assets/scripts/EnemyMovement.cs
+++ Assets/scripts/EnemyMovement.cs
@@ -11,7 +11,46 @@ public class EnemyMovement : MonoBehaviour
     public float enemySpeed = 2f;
     public Rigidbody2D rb;
 
+    public bool followingPlayer = false;
+    public bool moveRight;
+
+    public float playerDetectionTime;
+    public float timeBeforeLosingPlayer;
+
+    float moveOrIdleFloat;
+    public bool moving;
+    public bool idling;
+
+    public float minMoveTime;
+    public float maxMoveTime;
+
+    public float timeToMove;
+
+    float _stateTimer;
+    public float stateTimer
+    {
+        get
+        {
+            return _stateTimer;
+        }
+
+        private set
+        {
+            _stateTimer = value;
+            if(stateTimer >= timeToMove)
+            {
+                stateTimer = 0;
+                timeToMove = Random.Range(minMoveTime, maxMoveTime);
+                moveOrIdleFloat = Random.value;
+                directionDecider = true;
+            }
+        }
+    }
+
     public bool playerIsToTheRight;
+
+
+    bool directionDecider = true;
 
     private void Start()
     {
@@ -19,11 +58,17 @@ public class EnemyMovement : MonoBehaviour
         player = FindObjectOfType<PlayerMovement>().gameObject;
 
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+        timeToMove = Random.Range(minMoveTime, maxMoveTime);
+        moveOrIdleFloat = Random.value;
+        
+        
     }
 
     private void Update()
     {
-        if(player.transform.position.x < transform.position.x)
+        #region OldMovement
+        if (player.transform.position.x < transform.position.x)
         {
             playerIsToTheRight = true;
         }
@@ -39,6 +84,67 @@ public class EnemyMovement : MonoBehaviour
         if (!playerIsToTheRight)
         {
             rb.velocity = new Vector2(enemySpeed, 0);
+        }
+        #endregion
+
+        stateTimer += Time.deltaTime;
+
+        if (moveOrIdleFloat <= 0.70)
+        {
+            idling = false;
+            moving = true;
+
+            if (directionDecider)
+            {
+                directionDecider = false;
+                float leftorright = Random.value;
+                moveRight = leftorright <= 0.5f ? true : false;
+            }
+        }
+        else if(moveOrIdleFloat > 0.70)
+        {
+            moving = false;
+            idling = true;
+        }
+
+        //visual cone kod, om spelaren är i visual cone, starta en playerdetectiontimer, om den når sitt värde så ska followingPlayer = true
+        //om spelaren var i visual cone och går ut, starta losingplayertimer
+
+        //tänker kanske att visual cone kan vara en cirkelsektor från fiendet utåt i "radius = visualrange" typ
+
+        //sebastian lague tutorial/video om visual cone
+
+        if (moveRight)
+        {
+            //riktad åt höger
+        }
+        else
+        {
+            //riktad åt vänster
+        }
+
+        if (moving)
+        {
+            if (followingPlayer)
+            {
+                //fiende chase kod (rör sig mot spelaren)
+            }
+            else
+            {
+                //fiende random movement kod
+                if (moveRight)
+                {
+                    rb.velocity = new Vector2(enemySpeed, 0);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(-enemySpeed, 0);
+                }
+            }
+        }
+        else if (idling)
+        {
+            rb.velocity = Vector2.zero;
         }
     }
 
---
+++ Assets/scripts/FieldOfViewEditor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.right, 360, fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+
+    /*
+    private void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.forward, Vector3.up, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fov.visibleTargets)
+        {
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+    */
+}
